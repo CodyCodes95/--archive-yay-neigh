45d6145573ddc1f53d8fdacd49677a34994b210a
Cody Partington <codythatsme@gmail.com>
2022-09-26 23:59:40 +1000

    initial commit

---
+++ src/components/FormDescription.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormDescription = ({ children }: any) => {
+  return (
+    <p className='mb-4 font-jaldi tracking-[1.05px]'>{children}</p>
+  )
+}
+
+export default FormDescription
