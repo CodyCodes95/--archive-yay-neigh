9c9e6e14a34bbf87133806fb467053c334cb0dbe
Cody Partington <codythatsme@gmail.com>
2022-10-09 13:37:54 +1100

    set onchange for inputs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -88,13 +88,13 @@ const Home: NextPage = () => {
               <label className="" htmlFor="">
                 Full Name*
               </label>
-              <input type="text" className="w-[85%]" />
+              <input type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} className="w-[85%] text-black" />
             </div>
             <div className="flex flex-col">
               <label className="" htmlFor="">
                 City/State*
               </label>
-              <input type="text" className="w-[85%]" />
+              <input type="text" value={cityState} onChange={(e) => setCityState(e.target.value)} className="w-[85%] text-black" />
             </div>
           </div>
           <div className="flex">
@@ -102,43 +102,43 @@ const Home: NextPage = () => {
               <label className="" htmlFor="">
                 Contact Email*
               </label>
-              <input type="text" className="w-[85%]" />
+              <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="w-[85%] text-black" />
             </div>
             <div className="flex flex-col">
               <label className="" htmlFor="">
                 Instagram Username*
               </label>
-              <input type="text" className="w-[85%]" />
+              <input type="text" value={instagramName} onChange={(e) => setInstagramName(e.target.value)} className="w-[85%] text-black" />
             </div>
           </div>
           <label className="p-2" htmlFor="">
             Vehicle Year/Make/Model*
           </label>
-          <input type="text" />
+          <input type="text" value={yearMakeModel} onChange={(e) => setYearMakeModel(e.target.value)} />
           <label className="p-2" htmlFor="">
             Engine Modifications*
           </label>
-          <input type="text" />
+          <input type="text" value={engineMods} onChange={(e) => setEngineMods(e.target.value)} />
           <label className="p-2" htmlFor="">
             Suspension Modifications*
           </label>
-          <input type="text" />
+          <input type="text" value={suspensionMods} onChange={(e) => setSuspensionMods(e.target.value)} />
           <label className="p-2" htmlFor="">
             Exterior Modifications*
           </label>
-          <input type="text" />
+          <input type="text" value={exteriorMods} onChange={(e) => setExteriorMods(e.target.value)} />
           <label className="p-2" htmlFor="">
             Interior Modifications*
           </label>
-          <input type="text" />
+          <input type="text" value={interiorMods} onChange={(e) => setInteriorMods(e.target.value)} />
           <label className="p-2" htmlFor="">
             Link to HD Photos*
           </label>
-          <input type="text" />
+          <input type="text" value={photosLink} onChange={(e) => setPhotosLink(e.target.value)} />
           <label className="p-2" htmlFor="">
             Photographer Instagram Username*
           </label>
-          <input type="text" />
+          <input type="text" value={photogUserName} onChange={(e) => setPhotogUsername(e.target.value)} />
           <input type="file" />
           <div className="p-2" />
           <button className="bg-white text-black w-[25%]">Submit</button>
